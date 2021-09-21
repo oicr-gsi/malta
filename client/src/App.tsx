7e6f59bd0cc99452485586da1a7262799d11855a
Vivek Alamuri <61337145+valamuri2020@users.noreply.github.com>
2021-09-21 10:22:00 -0400

    displaying data returned from api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,38 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Screen } from "./Screen";
+
+interface Members {
+  members: String[];
+}
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<Members>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
   useEffect(() => {
+    setLoading(true);
     fetch("/members")
       .then((res) => res.json())
       .then((data) => {
         setData(data);
         console.log(data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
-  return <div></div>;
+  return (
+    <ul>
+      {data && data["members"].map((item, idx) => <li key={idx}>{item}</li>)}
+    </ul>
+  );
 }
 
 export default App;

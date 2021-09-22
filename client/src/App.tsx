b6f45a7636ae8324e634f6dd1c46378e4eb37a19
Vivek Alamuri <61337145+valamuri2020@users.noreply.github.com>
2021-09-22 10:38:01 -0400

    removed unnecessary starter files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { Screen } from "./Screen";
 
 interface Members {
@@ -13,24 +11,22 @@ function App() {
   const [error, setError] = useState();
 
   useEffect(() => {
-    setLoading(true);
-    fetch("/pdf")
-      .then((res) => {
-        res.json();
-        console.log(res);
-        setData(data);
-        console.log(data);
-      })
-      .then((data) => {
-        // setData(data);
-        // console.log(data);
-      })
-      .catch((err) => {
-        setError(err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    // setLoading(true);
+    // fetch("/pdf")
+    //   .then((res) => {
+    //     res.json();
+    //     console.log(res);
+    //   })
+    //   .then((data) => {
+    //     setData(data);
+    //     console.log(data);
+    //   })
+    //   .catch((err) => {
+    //     setError(err);
+    //   })
+    //   .finally(() => {
+    //     setLoading(false);
+    //   });
   }, []);
 
   return (

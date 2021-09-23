2fd4793dfa5d8ea67bddf9593d7c1cfa0ae4c2ae
Vivek Alamuri <61337145+valamuri2020@users.noreply.github.com>
2021-09-23 10:04:16 -0400

    cleaned comments and added GammaSelection (Screen) component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Screen } from "./Screen";
-
-interface Members {
-  members: String[];
-}
+import { GammaSelection } from "./gammaSelection";
 
 function App() {
-  const [data, setData] = useState<Members>();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    // setLoading(true);
-    // fetch("/pdf")
-    //   .then((res) => {
-    //     res.json();
-    //     console.log(res);
-    //   })
-    //   .then((data) => {
-    //     setData(data);
-    //     console.log(data);
-    //   })
-    //   .catch((err) => {
-    //     setError(err);
-    //   })
-    //   .finally(() => {
-    //     setLoading(false);
-    //   });
-  }, []);
-
   return (
     <>
-      {/* <ul>
-        {data && data["members"].map((item, idx) => <li key={idx}>{item}</li>)}
-      </ul> */}
       <>hi</>
       <br />
-      <Screen />
+      <GammaSelection />
     </>
   );
 }

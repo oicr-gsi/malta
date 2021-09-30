d68b486105073b88966cb142b4a73a4449e50d83
Vivek Alamuri <61337145+valamuri2020@users.noreply.github.com>
2021-09-30 15:42:08 -0400

    working on "permission denied" issue

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,34 +1,41 @@
 from flask import Flask, send_file, request
-from extraction import get_gamma_data, get_data_folders, get_gamma_options
+from extraction import get_gamma_data, get_data_folders, get_gamma_options, get_solution_name
 import os
 
 app = Flask(__name__)
 
-SOLUTION_FILENAME = str(os.getenv("SOLUTION_FILENAME"))
 
-@app.route("/data_options")
+SELECTED_DATA_FOLDER = ""
+
+@app.route("/data_folders")
 def data_options():
     return {"data": get_data_folders()}
 
-@app.route("/gamma_options")
-def gamma_options():
-    # this function will need to take a file path argument later on
-    DATA_PATH = os.path.join(os.getenv("MALTA_DATA_FOLDER"), str(os.getenv("TEST_DATA")))
-    return {"options": get_gamma_options(DATA_PATH)}
+@app.route("/selected_folder/<string:folder_name>", methods=["POST"])
+def selected_folder(folder_name):
+    if request.method == "POST":
+        print("\nselected folder:", folder_name, "\n")
+        SELECTED_DATA_FOLDER = folder_name
+        return gamma_options(SELECTED_DATA_FOLDER)
 
-@app.route("/data/<int:gamma>", methods=['POST'])
+
+def gamma_options(folder_name):
+    data_path = os.path.join(os.getenv("MALTA_DATA_FOLDER"), folder_name)
+    return {"options": get_gamma_options(data_path)}
+
+@app.route("/data/<int:gamma>", methods=["POST"])
 def data(gamma):
-    if request.method == 'POST':
+    if request.method == "POST":
         print("gamma from front end", gamma)
-        DATA_PATH = os.path.join(os.getenv("MALTA_DATA_FOLDER"), str(os.getenv("TEST_DATA")))
-        BASE_PATH = os.path.join(DATA_PATH, "gammas")
-        data = []
-        data = get_gamma_data(gamma, BASE_PATH)
+        # DATA_PATH = os.path.join(os.getenv("MALTA_DATA_FOLDER"), SELECTED_DATA_FOLDER)
+        # BASE_PATH = os.path.join(DATA_PATH, "gammas")
+        solution_filename = get_solution_name(SELECTED_DATA_FOLDER)
+        data = get_gamma_data(gamma, str(os.getenv("TEST_DATA")), get_solution_name(str(os.getenv("TEST_DATA"))) )
         return {gamma: data}
 
-@app.route("/pdf", methods=['POST'])
+@app.route("/pdf", methods=["POST"])
 def send_pdf():
-    if request.method == 'POST':
+    if request.method == "POST":
         path = request.get_json(force=True)
         print(path)
         return send_file(path)  
@@ -37,3 +44,4 @@ def send_pdf():
 if __name__ == "__main__":
     app.run(debug=True)
 
+# PANX_1288_Pm_M_100_JHU_004_LCM3_6.results.zip

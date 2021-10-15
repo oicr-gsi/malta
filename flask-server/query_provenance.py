6565c670e66270ab9b33e51d8064b74fb1b94efa
Vivek Alamuri <61337145+valamuri2020@users.noreply.github.com>
2021-10-15 15:37:56 -0400

    Update query_provenance.py

--- flask-server/query_provenance.py
+++ flask-server/query_provenance.py
@@ -9,6 +9,9 @@ STUDIES = os.getenv("STUDIES").split() if os.getenv("STUDIES") != None else ["PA
 dropdown_count = int(os.getenv("DROPDOWN_COUNT")) if os.getenv("DROPDOWN_COUNT") != None else 15
 provenance_file = str(os.getenv("LATEST_PROVENANCE"))
 
+# turn off SettingWithCopy warning thrown by false positive dataframe copies
+pd.options.mode.chained_assignment = None
+
 # relevant only when using a reduced file: cut -f 1,2,31,47 
 # column_names = ["Last Modified", "Study Title", "Workflow Name", "File Path"]
 df = pd.read_csv(provenance_file, delimiter="\t")

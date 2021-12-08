24467a7818bdd5bb1f1e5d7a005d53aaf1ff0d25
Vivek Alamuri <61337145+valamuri2020@users.noreply.github.com>
2021-12-08 11:04:39 -0500

    turned expected results into a module - cleaner code

--- flask-server/expected_results.py
+++ flask-server/expected_results.py
@@ -4,76 +4,81 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-expected_folders = [
-    "PANX_1249_Lv_M_100-PM-013_LCM5.results.zip",
-    "PANX_1277_Lv_M_100-JHU-003_LCM3.zip",
-    "PANX_1280_Lv_M_100-PM-022_LCM2.results.zip",
-    "PANX_1288_Pm_M_100_JHU_004_LCM3_6.results.zip",
-    "PANX_1295_Pm_M_WG_100-NH-002_LCM4_results.zip",
-    "PANX_1296_Pa_P_WG_100-PM-025_LCM6_results.zip",
-]
 
-expected_options = [
-    50,
-    100,
-    200,
-    300,
-    400,
-    500,
-    600,
-    700,
-    800,
-    900,
-    1000,
-    1250,
-    1500,
-    2000,
-]
+class ExpectedResults:
+    def __init__(self):
+        self.expected_folders = [
+            "PANX_1249_Lv_M_100-PM-013_LCM5.results.zip",
+            "PANX_1277_Lv_M_100-JHU-003_LCM3.zip",
+            "PANX_1280_Lv_M_100-PM-022_LCM2.results.zip",
+            "PANX_1288_Pm_M_100_JHU_004_LCM3_6.results.zip",
+            "PANX_1295_Pm_M_WG_100-NH-002_LCM4_results.zip",
+            "PANX_1296_Pa_P_WG_100-PM-025_LCM6_results.zip",
+        ]
 
-expected_solution_filename = "PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf"
+        self.expected_options = [
+            50,
+            100,
+            200,
+            300,
+            400,
+            500,
+            600,
+            700,
+            800,
+            900,
+            1000,
+            1250,
+            1500,
+            2000,
+        ]
 
-expected_extracted_text_data = {
-    "id": 0,
-    "path": os.path.join(
-        os.getenv("MALTA_OUTPUT_FOLDER"),
-        "gammas/100/PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf",
-    ),
-    "cellularity": 0.61,
-    "ploidy": 2.0,
-    "sd.BAF": 0.07,
-}
+        self.expected_solution_filename = (
+            "PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf"
+        )
 
-expected_alt_folders = ["gammas/100/sol3_0.3/", "gammas/100/sol2_0.44/"]
+        self.expected_extracted_text_data = {
+            "id": 0,
+            "path": os.path.join(
+                os.getenv("MALTA_OUTPUT_FOLDER"),
+                "gammas/100/PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf",
+            ),
+            "cellularity": 0.61,
+            "ploidy": 2.0,
+            "sd.BAF": 0.07,
+        }
 
-expected_data = [
-    {
-        "id": 0,
-        "path": os.path.join(
-            os.getenv("MALTA_OUTPUT_FOLDER"),
-            "gammas/100/PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf",
-        ),
-        "cellularity": 0.61,
-        "ploidy": 2.0,
-        "sd.BAF": 0.07,
-    },
-    {
-        "id": 1,
-        "path": os.path.join(
-            os.getenv("MALTA_OUTPUT_FOLDER"),
-            "gammas/100/sol3_0.3/PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf",
-        ),
-        "cellularity": 0.3,
-        "ploidy": 2.7,
-        "sd.BAF": 0.07,
-    },
-    {
-        "id": 2,
-        "path": os.path.join(
-            os.getenv("MALTA_OUTPUT_FOLDER"),
-            "gammas/100/sol2_0.44/PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf",
-        ),
-        "cellularity": 0.44,
-        "ploidy": 3.9,
-        "sd.BAF": 0.07,
-    },
-]
+        self.expected_alt_folders = ["gammas/100/sol3_0.3/", "gammas/100/sol2_0.44/"]
+
+        self.expected_data = [
+            {
+                "id": 0,
+                "path": os.path.join(
+                    os.getenv("MALTA_OUTPUT_FOLDER"),
+                    "gammas/100/PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf",
+                ),
+                "cellularity": 0.61,
+                "ploidy": 2.0,
+                "sd.BAF": 0.07,
+            },
+            {
+                "id": 1,
+                "path": os.path.join(
+                    os.getenv("MALTA_OUTPUT_FOLDER"),
+                    "gammas/100/sol3_0.3/PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf",
+                ),
+                "cellularity": 0.3,
+                "ploidy": 2.7,
+                "sd.BAF": 0.07,
+            },
+            {
+                "id": 2,
+                "path": os.path.join(
+                    os.getenv("MALTA_OUTPUT_FOLDER"),
+                    "gammas/100/sol2_0.44/PANX_1280_Lv_M_WG_100-PM-022_LCM2_model_fit.pdf",
+                ),
+                "cellularity": 0.44,
+                "ploidy": 3.9,
+                "sd.BAF": 0.07,
+            },
+        ]
--- flask-server/test_extraction.py
+++ flask-server/test_extraction.py
@@ -12,31 +12,21 @@ from extraction import (
     get_alternate_solutions,
     get_gamma_data,
 )
-from expected_results import (
-    expected_folders,
-    expected_options,
-    expected_solution_filename,
-    expected_extracted_text_data,
-    expected_alt_folders,
-    expected_data,
-)
+from expected_results import ExpectedResults
 
-# from expected_results import ExpectedResults
+exp_res = ExpectedResults()
 
 
 class TestExtraction(unittest.TestCase):
-    # def __init__(self):
-    #     self.exp_res = ExpectedResults()
-
     def test_get_data_folders(self):
-        self.assertEqual(get_data_folders(), expected_folders)
+        self.assertEqual(get_data_folders(), exp_res.expected_folders)
 
     def test_get_gamma_options(self):
         load_dotenv()
         path = os.path.join(
             os.getenv("MALTA_DATA_FOLDER"), str(os.getenv("MALTA_TEST_DATA"))
         )
-        self.assertEqual(get_gamma_options(path), expected_options)
+        self.assertEqual(get_gamma_options(path), exp_res.expected_options)
 
     def test_set_path(self):
         gamma = 200
@@ -46,7 +36,8 @@ class TestExtraction(unittest.TestCase):
     def test_get_solution_name(self):
         folder = str(os.getenv("MALTA_TEST_DATA"))
         self.assertEqual(
-            get_solution_name(folder, "_model_fit.pdf"), expected_solution_filename
+            get_solution_name(folder, "_model_fit.pdf"),
+            exp_res.expected_solution_filename,
         )
 
     def test_extract(self):
@@ -58,16 +49,18 @@ class TestExtraction(unittest.TestCase):
             # always fails this test if path is also checked
             actual = extract_text(path, 0, folder, tmpdir)
             del actual["path"]
-            del expected_extracted_text_data["path"]
+            del exp_res.expected_extracted_text_data["path"]
 
-            self.assertEqual(actual, expected_extracted_text_data)
+            self.assertEqual(actual, exp_res.expected_extracted_text_data)
 
     def test_get_alternate_solutions(self):
         load_dotenv()
         gamma = 100
         folder = str(os.getenv("MALTA_TEST_DATA"))
         self.assertTrue(type(get_alternate_solutions(gamma, folder) == list))
-        self.assertEqual(get_alternate_solutions(gamma, folder), expected_alt_folders)
+        self.assertEqual(
+            get_alternate_solutions(gamma, folder), exp_res.expected_alt_folders
+        )
 
     def test_get_gamma_data(self):
         gamma = 100
@@ -79,10 +72,10 @@ class TestExtraction(unittest.TestCase):
             actual = get_gamma_data(gamma, folder, tmpdir)
             for d in actual:
                 del d["path"]
-            for e in expected_data:
+            for e in exp_res.expected_data:
                 del e["path"]
             self.assertTrue(type(get_gamma_data(gamma, folder, tmpdir) == list))
-            self.assertEqual(actual, expected_data)
+            self.assertEqual(actual, exp_res.expected_data)
 
 
 if __name__ == "__main__":
